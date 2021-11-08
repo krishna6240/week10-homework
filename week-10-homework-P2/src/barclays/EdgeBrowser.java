34b0a81365afa8f6364495e7ea0e143bbb6e9aa9
unknown <krishna6240@gmail.com>
2021-11-08 20:38:21 +0000

    adding 5 project for week 10 homework

---
+++ week-10-homework-P2/src/barclays/EdgeBrowser.java
@@ -0,0 +1,18 @@
+package barclays;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class EdgeBrowser {
+    public static void main(String[] args) {
+        String baseUrl = "https://www.barclays.co.uk/";
+        System.setProperty("webdriver.edge.driver","drivers/msedgedriver.exe");
+        WebDriver driver = new EdgeDriver();
+        driver.get(baseUrl);
+        driver.manage().window().maximize();
+        String getTitle = driver.getTitle();
+        System.out.println(getTitle);
+        driver.close();
+    }
+
+}
---
+++ week-10-homework-p1/src/nopcommerce/FireFoxBrowser.java
@@ -0,0 +1,19 @@
+package nopcommerce;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FireFoxBrowser {
+    public static void main(String[] args) {
+        String baseUrl = "https://demo.nopcommerce.com/";
+        //String baseUrl = "https://www.google.com/";
+        System.setProperty("webdriver.gecko.driver","drivers/geckodriver.exe");
+        WebDriver driver =  new FirefoxDriver();
+        driver.get(baseUrl);
+        driver.manage().window().maximize();//maximise
+        String getTitle = driver.getTitle();
+        System.out.println(getTitle);
+        driver.close();
+    }
+
+}
